14921ee64bdcc39ebfdc700515ead6ba9115c176
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-07-29 11:00:13 +0100

    first GET handler tested

--- app.js
+++ app.js
@@ -17,3 +17,20 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+// | GET   |     | | all recipes | { success: Boolean, payload: recipe array } |
+
+app.get("/api/recipes", async (req, res) => {
+  try {
+      //const payload = await getRecipes();
+      const payload = "it worked";
+      res.status(200).json({
+          "success": true,
+          "payload": payload
+      });
+  } catch (error) {
+      res.status(404).json({
+          "error": error.message
+      });
+  }
+});
