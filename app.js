582fd3a3cdaa8af3a38454b196dca67f2e958180
Kellytheengineer <77806498+Kellytheengineer@users.noreply.github.com>
2024-07-29 12:31:13 +0100

    first task complete

--- app.js
+++ app.js
@@ -52,3 +52,83 @@ res.status(404).json({
   
  }
 });
+
+//| POST   | /api/recipes | { body } | create a new recipe | { success: Boolean, payload: recipe }       |
+// ()   {}   []   ‘`
+app.post('/api/recipes' ,async(req, res) => {
+  const newRecipe = req.body; 
+  try {
+  //const payload = "it worked";
+  const payload = `your new recipe is ${newRecipe}`;
+    res.status(200).json({
+      "success": true,
+      "payload": payload
+    }); 
+  }
+  catch (error) {
+      res.status(400).json({
+        "error": error.message
+      });
+    }
+  });
+
+//| PATCH  | /api/recipes/:id | { body }   | updated recipe | { success: Boolean, payload: recipe } 
+app.patch('/api/recipes/:id',async(req, res) => {
+  const updateRecipe = req.body;
+  const id = req.params.id; 
+  try { 
+    const payload = `your ID is ${id}`;  
+    res.status(200).json({
+    "success": true,
+    "payload": payload
+  }); 
+  }
+  catch (error) {
+    res.status(400).json({
+      "error": error.message
+    });
+  }
+});
+
+// | DELETE | /api/recipes/:id | | { success: Boolean, payload: recipe }       |
+app.delete('/api/recipes/:id' , async(req, res) => {
+  const deleteRecipe = req.body;
+  const id = req.params.id;
+try { 
+  const payload = `your ID is ${id}`;  
+  res.status(200).json({
+  "success": true,
+  "payload": payload
+}); 
+}
+catch (error) {
+  res.status(400).json({
+    "error": error.message
+  });
+}
+});
+
+
+/*app.post('/activities', (req, res) => {
+    const id = uuidv4();
+    const activity_submitted = Date.now();
+    const newActivity = {
+        id,
+        activity_submitted,
+        ...req.body
+    };  
+    if (!newActivity.activity_type){
+        res.status(400).json({ error: 'Failed because activity_type is required' });
+    }; 
+    if (!newActivity.activity_duration){
+        res.status(400).json({ error: 'Failed because activity_duration is required' });
+    }; 
+    if (typeof newActivity.activity_type !== 'string') {
+       return res.status(400).json({
+        error: 'Invalid request format. Ensure activity_type is a string .'});
+    };
+    if (typeof newActivity.activity_duration !== "number")  {
+        return res.status(400).json({
+            error: 'Invalid request format. Ensure activity_duration is a number .'
+        });
+    //};*/
